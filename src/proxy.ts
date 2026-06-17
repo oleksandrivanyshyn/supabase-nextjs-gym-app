d85b7202d4dc177eac086fe66f148cc4b8011087
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-17 22:46:18 +0300

    feat: enhance Clerk middleware to protect private routes via route matcher

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,19 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware()
+const isPrivateRoute = createRouteMatcher(['/account(.*)']);
 
+export default clerkMiddleware(async (auth, req) => {
+  if (isPrivateRoute(req)) {
+    await auth.protect();
+  }
+});
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-        // Always run for Clerk-specific frontend API routes
-        '/__clerk/(.*)',
-    ],
-}
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+    // Always run for Clerk-specific frontend API routes
+    '/__clerk/(.*)',
+  ],
+};
