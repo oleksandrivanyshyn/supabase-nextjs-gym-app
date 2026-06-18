73792294782214bf650d01067233ede8bfc03976
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-18 16:31:57 +0300

    feat: add `MenuItems` component for user/admin navigation and restructure `Header` with menu integration

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import { UserButton } from '@clerk/nextjs';
-import { currentUser } from '@clerk/nextjs/server';
-import { getCurrentUserFromSupabase } from '@/actions/users';
 
 const AccountPage = async () => {
-  const result = await currentUser();
-  const name = result?.firstName + ' ' + result?.lastName;
-  const clerkUserId = result?.id;
-  const email = result?.emailAddresses[0].emailAddress;
-  const response = await getCurrentUserFromSupabase();
   return (
-    <div className="p-5">
+    <div>
       <h1>Account Page</h1>
-      <UserButton fallback="/" />
-      <h1>Clerk User Name: {name}</h1>
-      <h1>Clerk User Email: {email}</h1>
-      <h1>Clerk User ID: {clerkUserId}</h1>
     </div>
   );
 };
--- src/custom-layout/header.tsx
+++ src/custom-layout/header.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
 import { IUser } from '@/interfaces';
 import { Menu } from 'lucide-react';
+import React from 'react';
+import MenuItems from './menu-items';
 
-const Header = ({ user }: { user: IUser | null }) => {
+function Header({ user }: { user: IUser | null }) {
+  const [openMenuItems, setOpenMenuItems] = React.useState<boolean>(false);
   return (
     <div className="flex items-center justify-between bg-primary px-5 py-6">
       <h1 className="text-2xl font-bold text-white">
@@ -13,10 +15,22 @@ const Header = ({ user }: { user: IUser | null }) => {
       <div className="flex gap-5 items-center">
         <h1 className="text-sm text-white">{user?.name}</h1>
 
-        <Menu className="text-white cursor-pointer" size={15} />
+        <Menu
+          className="text-white cursor-pointer"
+          size={15}
+          onClick={() => setOpenMenuItems(true)}
+        />
       </div>
+
+      {openMenuItems && user && (
+        <MenuItems
+          user={user}
+          openMenuItems={openMenuItems}
+          setOpenMenuItems={setOpenMenuItems}
+        />
+      )}
     </div>
   );
-};
+}
 
 export default Header;
