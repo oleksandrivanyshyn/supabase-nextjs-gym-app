4563f53aacee58a2ef86497658d270cd4982e716
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-18 21:30:24 +0300

    feat: enhance account page with user greeting, subscription plans link, and `PageTitle` integration

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,11 +1,34 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import PageTitle from '@/components/ui/page-title';
+import usersGlobalStore, {
+  IUsersGlobalStore,
+} from '@/global-store/users-store';
+import Link from 'next/link';
 import React from 'react';
 
-const AccountPage = async () => {
+function AccountPage() {
+  const { user } = usersGlobalStore() as IUsersGlobalStore;
+
   return (
     <div>
-      <h1>Account Page</h1>
+      <PageTitle title={`Welcome ${user?.name}`} />
+
+      <div className="flex flex-col gap-5">
+        <p className="mt-5 text-sm text-gray-600">
+          Welcome to your account panel. Here you can view your personal details
+          and available services.
+        </p>
+
+        <Button className="w-max">
+          <Link href={'/account/user/purchase-plan'}>
+            View Subscription Plans
+          </Link>
+        </Button>
+      </div>
     </div>
   );
-};
+}
 
 export default AccountPage;
