efb711c2802d1f73d9c33ceed27aa5296964940f
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-18 18:38:06 +0300

    feat: implement form components for plan management with validation and file upload

--- src/app/(private)/account/admin/plans/add/page.tsx
+++ src/app/(private)/account/admin/plans/add/page.tsx
@@ -1,10 +1,12 @@
 import PageTitle from '@/components/ui/page-title';
 import React from 'react';
+import PlanForm from '../_components/plan-form';
 
 function AddPlanPage() {
   return (
     <div>
       <PageTitle title="Add Plan" />
+      <PlanForm formType="add" initialValues={null} />
     </div>
   );
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,25 @@
+'use server';
+import supabase from '@/config/supabase-config';
+
+export const uploadFileAndGetUrl = async (file: File) => {
+  try {
+    const fileName = new Date().getTime() + file.name;
+    const { data, error } = await supabase.storage
+      .from('default')
+      .upload(fileName, file);
+    if (error) throw new Error(error.message);
+
+    const urlResponse = await supabase.storage
+      .from('default')
+      .getPublicUrl(fileName);
+    return {
+      success: true,
+      data: urlResponse.data.publicUrl,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
